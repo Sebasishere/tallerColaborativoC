8ac1174bdb460ab4b3ccd88578176ccc2c473b7e
Leibnitz Rojas <leibnitz.rojas@uptc.edu.co>
2022-12-21 15:33:04 -0500

    Matriz

---
+++ cmake-build-debug/CMakeFiles/proyectoC.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proyectoC.dir/main.cpp.obj"
+  "CMakeFiles/proyectoC.dir/main.cpp.obj.d"
+  "libproyectoC.dll.a"
+  "proyectoC.exe"
+  "proyectoC.exe.manifest"
+  "proyectoC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proyectoC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/57317/CLionProjects/proyectoC
+# Install script for directory: C:/Users/57310/CLionProjects/tallerColaborativoC
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/57317/CLionProjects/proyectoC/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/57310/CLionProjects/tallerColaborativoC/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
